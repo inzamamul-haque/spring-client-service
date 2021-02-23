e2d80f3dd856a6367b2f759b531659bf4d0e0e02
inzamamul.haque <inzamamul.rifat@gmail.com>
2021-02-23 12:09:50 +0600

    --add config client for eureka registry and spring basic security, update java version.

--- src/main/java/com/inzamamul/postgresapp/PostgresAppApplication.java
+++ src/main/java/com/inzamamul/postgresapp/PostgresAppApplication.java
@@ -2,10 +2,8 @@ package com.inzamamul.postgresapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PostgresAppApplication {
